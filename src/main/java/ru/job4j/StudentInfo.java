3e7eaac99202ab9442e49f80acf334763787f12e
nikolaevichdmitriy <git@githab.com>
2023-02-02 18:14:44 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -3,8 +3,8 @@ package ru.job4j;
 public class StudentInfo {
     public static void main(String[] args) {
 
-        System.out.printf("Dmitriy Dyadicyn");
-        System.out.printf("01.06.1981");
+        System.out.println("Dmitriy Dyadicyn");
+        System.out.println("01.06.1981");
 
     }
 }
