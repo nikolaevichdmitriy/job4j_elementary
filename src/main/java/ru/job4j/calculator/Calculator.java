7efa5d6f366f36aa51afb57a09c4134b6d11f882
nikolaevichdmitriy <124059539+nikolaevichdmitriy@users.noreply.github.com>
2023-02-05 15:56:51 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -15,9 +15,15 @@ public class Calculator {
         System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
         System.out.println(fourTimeTwo);
-
     }
 }
+
+
+
+
+
+
+
 
 
 
