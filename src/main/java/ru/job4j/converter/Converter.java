73b4b55a781ad6aaee650d5e01dfe4a22c99386e
nikolaevichdmitriy <124059539+nikolaevichdmitriy@users.noreply.github.com>
2023-02-06 15:48:14 +0300

    3.2. Загрузка файлов на github [#243562]

---
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,26 @@
+package ru.job4j.converter;
+
+public class Converter {
+    public static float rubleToEuro(float value) {
+        float rsl = value / 70;
+        return rsl;
+    }
+
+    public static float rubleToDollar(float value) {
+        float rsl = value / 60;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEuro(2000);
+        float dollar = Converter.rubleToDollar(2000);
+        System.out.println(euro + " " + "euro");
+        System.out.println(dollar + " " + "rubles");
+    }
+}
+
+
+
+
+
+
