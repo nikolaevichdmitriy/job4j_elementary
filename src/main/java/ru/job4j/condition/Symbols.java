848ffaba9534919f30eebe7cef115f015f658c07
nikolaevichdmitriy <124059539+nikolaevichdmitriy@users.noreply.github.com>
2023-02-08 16:12:11 +0300

    3.2. Загрузка файлов на github [#243562]

---
+++ src/main/java/ru/job4j/condition/Symbols.java
@@ -0,0 +1,10 @@
+package ru.job4j.condition;
+
+public class Symbols {
+    public static void main(String[] args) {
+        char a = 'A';
+        System.out.println(++a);
+        char d = 'D';
+        System.out.println(--d);
+    }
+}
