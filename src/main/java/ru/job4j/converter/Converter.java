3291df9d1b248c517cbf04ce3edad7b2266eb3cc
nikolaevichdmitriy <124059539+nikolaevichdmitriy@users.noreply.github.com>
2023-02-09 06:53:15 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -21,11 +21,11 @@ public class Converter {
         float out = Converter.rubleToEuro(in);
         boolean passed = expected == out;
         System.out.println("140 rubles are 2. Test result : " + passed);
-        float in1 = 140;
-        float expected1 = 2;
-        float out1 = Converter.rubleToDollar(in1);
-        boolean passed1 = expected1 == out1;
-        System.out.println("140 rubles are 2. Test result : " + passed1);
+         in = 140;
+         expected =  2;
+         out = Converter.rubleToDollar(in);
+         passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
     }
 }
 
