59574761304dfc9e861c6109f93c08377a0620dd
nikolaevichdmitriy <124059539+nikolaevichdmitriy@users.noreply.github.com>
2023-02-12 09:40:14 +0300

    3.2. Загрузка файлов на github [#243562]

---
+++ src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.condition;
+import org.junit.Assert;
+import org.junit.Test;
+public class SqAreaTest {
+    @Test
+    public void whenP6K2Square2() {
+        int expected = 2;
+        int p = 6;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP7K1Square30625() {
+        double expected = 3.0625;
+        int p = 7;
+        double k = 1;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP7K2Square27222222222222228() {
+        double expected = 2.7222222222222228;
+        int p = 7;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenP7K2Square0() {
+        int  expected = 0;
+        int p = 7;
+        double k = 0;
+        double out = SqArea.square(p, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
+
+
+
+
+
+
