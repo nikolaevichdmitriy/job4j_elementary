40e452a4cb82df3f50a1c1d97a6b43833c42b26d
nikolaevichdmitriy <124059539+nikolaevichdmitriy@users.noreply.github.com>
2023-02-12 07:05:37 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -10,10 +10,44 @@ public class X2Test {
         int a = 1;
         int b = 1;
         int c = 1;
+        int x = 1;
+        int expected = 3;
+        int actual = X2.calc(1, 1, 1, 1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void calc1() {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 2;
+        int actual = X2.calc(0, 1, 1, 1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void calc2() {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected = 2;
+        int actual = X2.calc(1, 1, 0, 1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void calc3() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
         int x = 0;
         int expected = 1;
         int actual = X2.calc(1, 1, 1, 0);
         Assert.assertEquals(expected, actual);
     }
-
 }
+
+
