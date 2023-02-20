16c2842a45b65a7825d804662e35b920984b9d2a
nikolaevichdmitriy <124059539+nikolaevichdmitriy@users.noreply.github.com>
2023-02-20 16:07:05 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/condition/LogicNot.java
+++ src/main/java/ru/job4j/condition/LogicNot.java
@@ -10,19 +10,19 @@ public class LogicNot {
     }
 
     public static boolean notEven(int num) {
-        return !(num % 2 == 0);
+        return !isEven(num);
     }
 
     public static boolean notPositive(int num) {
-        return !(num > 0);
+        return num < 0;
     }
 
     public static boolean notEvenAndPositive(int num) {
-        return !(num % 2 == 0) && num > 0;
+        return notEven(num) && isPositive(num);
     }
 
     public static boolean evenOrNotPositive(int num) {
-        return num % 2 == 0 || !(num > 0);
+        return isEven(num) || notPositive(num);
     }
 
     public static void main(String[] args) {
