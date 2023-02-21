0a7187635dea8109e9aece8779e400898c7ef092
nikolaevichdmitriy <124059539+nikolaevichdmitriy@users.noreply.github.com>
2023-02-21 07:16:37 +0300

    3.2. Загрузка файлов на github [#243562]

---
+++ src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Triangle {
+    public static boolean exist(double ab, double ac, double bc) {
+        return  ab + ac > bc || ac + bc > ab && bc + ab > ac;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Triangle.exist(0, 0, 0));
+    }
+}
