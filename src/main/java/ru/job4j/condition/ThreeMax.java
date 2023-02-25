33ab688e4d5b0337d013bf465808a92ed9ec9547
nikolaevichdmitriy <124059539+nikolaevichdmitriy@users.noreply.github.com>
2023-02-25 09:24:54 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/condition/ThreeMax.java
+++ src/main/java/ru/job4j/condition/ThreeMax.java
@@ -2,12 +2,13 @@ package ru.job4j.condition;
 
 public class ThreeMax {
     public static int max(int first, int second, int third) {
-        int result = second;
-        if (first > second && first > third) {
+        int result = first;
+        if (first >= second && first >= third) {
+            result = first;
+        } else if (second >= first && second >= third) {
             result = second;
-        }
-        if (second > first && second > third) {
-            result = second;
+        } else {
+            return third;
         }
         return result;
     }
