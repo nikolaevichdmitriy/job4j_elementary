fe1737f6d214b15ec0553aca6a4629f02db8b7ee
nikolaevichdmitriy <124059539+nikolaevichdmitriy@users.noreply.github.com>
2023-02-28 19:38:50 +0300

    3.2. Загрузка файлов на github [#243562]

---
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CounterTest {
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenSumEvenNumbersFromZeroToTenThenThirty() {
+        int start = 0;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+}
