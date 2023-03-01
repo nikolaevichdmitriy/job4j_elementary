9300a68477a72cdf17858220d0b8444f1a0a2eb7
nikolaevichdmitriy <124059539+nikolaevichdmitriy@users.noreply.github.com>
2023-03-01 14:57:44 +0300

    3.2. Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -3,17 +3,17 @@ package ru.job4j.loop;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        for (int i = 0; i <= finish; i++) {
-            sum = sum + i;
+        for (start = 0; start <= finish; start++) {
+            sum += start;
         }
         return sum;
     }
 
     public static int sumByEven(int start, int finish) {
         int sum = 0;
-        for (int i = 1; i <= finish; i++) {
-            if (i % 2 <= 0) {
-                sum = sum + i;
+        for (start = 1; start <= finish; start++) {
+            if (start % 2 == 0) {
+                sum += start;
             }
         }
         return sum;
