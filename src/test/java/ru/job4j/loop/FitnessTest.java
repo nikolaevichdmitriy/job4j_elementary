e6c588ad3baecd7e3bb9afc7afbbd08e26133237
nikolaevichdmitriy <124059539+nikolaevichdmitriy@users.noreply.github.com>
2023-03-04 08:23:18 +0300

    5.4. Протеиновая диета (цикл while) [#156313 #397350]

--- src/test/java/ru/job4j/loop/FitnessTest.java
+++ src/test/java/ru/job4j/loop/FitnessTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.assertj.core.api.Assertions.assertThat;
     public class FitnessTest {
+
         @Test
         public void whenIvanGreatNik() {
             int ivan = 95;
