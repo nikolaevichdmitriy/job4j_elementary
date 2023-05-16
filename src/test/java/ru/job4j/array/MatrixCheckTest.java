79b6c456409b026fb3b24f4118cfd1ead357115d
nikolaevichdmitriy <124059539+nikolaevichdmitriy@users.noreply.github.com>
2023-05-16 20:52:15 +0300

    6.7.3. Массив из диагонали матрицы.  [#214128]

--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -2,7 +2,6 @@ package ru.job4j.array;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 import static org.assertj.core.api.Assertions.assertThat;
 public class MatrixCheckTest {
     @Test
@@ -41,4 +40,41 @@ public class MatrixCheckTest {
         boolean result = MatrixCheck.monoVertical(input, column);
         assertThat(result).isTrue();
     }
-}
+
+        @Test
+        public void whenDiagonalFullX() {
+            char[][] input = {
+                    {'X', ' ', ' '},
+                    {' ', 'X', ' '},
+                    {' ', ' ', 'X'},
+            };
+            char[] result = MatrixCheck.extractDiagonal(input);
+            char[] expected = {'X', 'X', 'X'};
+            assertThat(result).containsExactly(expected);
+        }
+
+        @Test
+        public void whenDiagonalFullOne() {
+            char[][] input = {
+                    {'1', ' ', ' '},
+                    {' ', '1', ' '},
+                    {' ', ' ', '1'},
+            };
+            char[] result = MatrixCheck.extractDiagonal(input);
+            char[] expected = {'1', '1', '1'};
+            assertThat(result).containsExactly(expected);
+        }
+
+        @Test
+        public void whenDiagonalMix() {
+            char[][] input = {
+                    {'X', ' ', ' '},
+                    {' ', 'Y', ' '},
+                    {' ', ' ', 'Z'},
+            };
+            char[] result = MatrixCheck.extractDiagonal(input);
+            char[] expected = {'X', 'Y', 'Z'};
+            assertThat(result).containsExactly(expected);
+        }
+    }
+
